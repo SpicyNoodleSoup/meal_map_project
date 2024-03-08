1c44de49a40c66a5ce62cfa322f2461ea289e033
“Xiaoshi <2742059c@student.gla.ac.uk>
2024-03-08 16:56:11 +0000

    Sign up created

--- meal_map_project/meal_map/admin.py
+++ meal_map_project/meal_map/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from meal_map.models import UserProfile
 
 # Register your models here.
+admin.site.register(UserProfile)
--- meal_map_project/meal_map/views.py
+++ meal_map_project/meal_map/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView, DetailView
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Restaurant
+from .forms import UserForm, UserProfileForm
 
 # This view is used to take all the restaurant objects in the database and
 # show it in a list formate
@@ -55,8 +56,34 @@ def my_reviews(request):
     return response
 
 def register(request):
-    response = render(request, 'meal_map/register.html')
-    return response
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            if 'picture' in request.FILES:
+                profile.picture = request.FILES['picture']
+            profile.save()
+            registered = True
+        else:
+            print(user_form.errors, profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = UserProfileForm()
+    
+    return render(request,
+        'meal_map/register.html',
+         context = {'user_form': user_form,
+        'profile_form': profile_form,
+        'registered': registered})
 
 def restaurant(request):
     response = render(request, 'meal_map/restaurant.html')
