4840088718d80ca90f18883c5ffc9099546f4ba7
Abdisalam Abukar <2618966a@student.gla.ac.uk>
2024-03-15 14:38:00 +0000

    Admin interface Update

--- meal_map_project/meal_map/admin.py
+++ meal_map_project/meal_map/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from meal_map.models import UserProfile
+from meal_map.models import UserProfile, Restaurant, Review, Reviewer
+
 
 # Register your models here.
 admin.site.register(UserProfile)
+
+admin.site.register(Reviewer)
+
+admin.site.register(Restaurant)
+
+admin.site.register(Review)
+
